2e341b6482335e78737fad7d58540b5f699f765b
arthurtozo <95719805+arthurtozo@users.noreply.github.com>
2023-08-22 10:24:09 -0300

    Modificado

---
+++ ex006.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número: '))
+print(f'O dobro de {n} vale {2*n}')
+print(f'O triplo de {n} vale {3*n}. \nA raiz quadrada de {n} vale {n**(1/2):.2f}')
+print(f'A raiz quadrada de {n} vale {pow(n, (1/2)):.2f}')
