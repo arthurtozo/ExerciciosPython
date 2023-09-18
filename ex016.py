9c10d44edc65943d47fe802bd131570a310a432c
arthurtozo <95719805+arthurtozo@users.noreply.github.com>
2023-09-18 10:37:42 -0300

    Modificado

---
+++ ex016.py
@@ -0,0 +1,6 @@
+'''from math import trunc
+num = float(input('Digite um valor: '))
+print(f'O valor digitado foi {num} e a sua porção inteira é {trunc(num)}')'''
+
+num = float(input('Digite um valor: '))
+print(f'O valor digitado foi {num} e a sua porção inteira é {int(num)}')
